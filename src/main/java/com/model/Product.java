aae99d5f9b33474d990eb9fe539207604488b6f8
Sudershan Khadka <106295087+sudershankhhadka@users.noreply.github.com>
2024-02-21 22:34:46 +0545

    ok

---
+++ src/main/java/com/model/Product.java
@@ -0,0 +1,52 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="prosuct_table")
+public class Product {
+	@Id //PK
+	@GeneratedValue //auto increment
+	@Column(name= "product_name",length = 100)
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private String company;
+	@Column
+	private int price;
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", company=" + company + ", price=" + price + "]";
+	}
+	
+}
